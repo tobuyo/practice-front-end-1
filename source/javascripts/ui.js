d2135e41b78cda20e90e3fca70bf7c83f516e78b
隼人明渡 <lllll.skyline.lllll@gmail.com>
2017-05-24 14:09:57 +0900

    改善インデント

--- source/javascripts/ui.js
+++ source/javascripts/ui.js
@@ -2,7 +2,8 @@ $(function(){
 	$("#mod-accordion dt").on("click", function() {
 		$(this).next().slideToggle();
 	});
-		$(".tab li").click(function() {
+
+	$(".tab li").click(function() {
 		var index = $(".tab li").index(this);
 		$(".content li").addClass("is-closed");
 		$(".content li").eq(index).removeClass("is-closed");
